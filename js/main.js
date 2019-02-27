d72d0b03a44998875781d84074a737f9ed9f923a
Scott Roberts <s.e.roberts0@gmail.com>
2019-02-26 21:50:31 -0800

    Fix loader and responsive search

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import MessageBoardApp from './components/MessageBoardApp.js';
 import CommentList from './components/CommentList.js';
 import CommentItem from './components/CommentItem.js';
+import Loader from './components/Loader.js';
 
 customElements.define('message-board-app', MessageBoardApp);
 customElements.define('message-board-comment-list', CommentList);
 customElements.define('message-board-comment-item', CommentItem);
+customElements.define('message-board-loader', Loader);
